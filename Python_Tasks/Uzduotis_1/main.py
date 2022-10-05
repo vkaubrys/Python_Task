0d44ecfd6069d44bde9dc09ad957d36d8e6000f9
vkaubrys <102465736+vkaubrys@users.noreply.github.com>
2022-10-05 22:01:06 +0300

    Add files via upload

--- Python_Tasks/Uzduotis_1/main.py
+++ Python_Tasks/Uzduotis_1/main.py
@@ -25,35 +25,24 @@ users = [
     { 'id': '9', 'name': 'Daniel Cane', 'age': 15, 'hasDog': False, 'hasCat': False },
 ]
 
-# def filter_all_or_nothing_people (users):
-#     if users['hasDog' and 'hasCat'] == False:
-#         return True
-#     else:
-#         return False
-# new_users = filter(filter_all_or_nothing_people, users)
-# for i in new_users:
-#     print(dict(i)['name'])
 
-def filter_all_or_nothing_people (users):
-
-   for x in users:
-       if (x['hasDog']==True and x['hasCat']==False) or (x['hasDog']==False and x['hasCat']==True):
-           users.remove(x)
-   return users
 
 def filter_underaged_owners (users):
+    res = list(filter(lambda i: (i['hasDog']==True and i['hasCat']==True) or (i['hasDog']==False and i['hasCat']==False), users))
+    return res
 
-    for x in users:
-        if (x['age']>18) or (x['age']>18 and x['hasDog'==False]):
-            users.remove(x)
-    return users
+def filter_all_or_nothing_people (users):
+    res = list(filter(lambda i: i['age'] < 18 and (i['hasDog']==True or i['hasCat']==True), users))
+    return res
 
-print("filter_all_or_nothing_people")
-users1 = filter_all_or_nothing_people (users)
+
+print ("filter_underaged_owners")
+users1 = filter_underaged_owners (users)
 for x in users1:
     print(x)
-print("filter_underaged_owners")
-users2=filter_underaged_owners (users)
+
+print ("filter_all_or_nothing_people")
+users2 = filter_all_or_nothing_people (users)
 for x in users2:
     print(x)
 
--- Python_Tasks/Uzduotis_4/main.py
+++ Python_Tasks/Uzduotis_4/main.py
@@ -15,3 +15,32 @@
 # Metodas:
 # * was_expensive() - jeigu filmo "budget" yra daugiau nei 100 mln. USD,
 #   grąžins True, kitu atveju - False.
+
+
+
+class Movie:
+    title =""
+    director =""
+    budget = 0
+    def was_expensive(self):
+        res = False
+        if self.budget>100000000:
+         res = True
+        return res
+
+
+Movie1 = Movie()
+Movie1.title = "Titanic"
+Movie1.director = "James Cameron"
+Movie1.budget = 60000000000
+
+Movie2 = Movie()
+Movie2.title = "Tadas Blinda"
+Movie2.director = "Donatas Ulvydas"
+Movie2.budget = 100000
+
+
+print(Movie1.was_expensive())
+print(Movie2.was_expensive())
+
+
